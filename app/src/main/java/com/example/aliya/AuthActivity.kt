4f314a8f997ec848d527f4aae9d19036ab5c8ce4
nasywa <nasywa24si@mahasiswa.pcr.ac.id>
2026-04-22 08:33:49 +0700

    pertemuan6 SharedPrefences dan isLogin

---
+++ app/src/main/java/com/example/aliya/AuthActivity.kt
@@ -0,0 +1,58 @@
+package com.example.aliya
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aliya.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Kode ini harus selalu dipanggil saat butuh akses "user_pref"
+        val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
+
+        //Kondisi jika isLogin bernilai true
+        val isLogin = sharedPref.getBoolean("isLogin", false)
+        if (isLogin) {
+            //Panggil Intent untuk ke MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (username.isNotEmpty() && username == password) {
+                val editor = sharedPref.edit()
+                editor.putBoolean("isLogin", true)
+                editor.putString("username",username)
+                editor.apply()
+
+                // Menggunakan Toast agar muncul notifikasi tanpa harus menekan tombol OK/Lanjut
+                Toast.makeText(this, "Login Berhasil! Selamat datang $username", Toast.LENGTH_SHORT).show()
+                
+                // Langsung berpindah ke MainActivity (Dashboard)
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                // Alert Gagal tetap menggunakan AlertDialog agar user menyadari kesalahan
+                AlertDialog.Builder(this)
+                    .setTitle("Login Gagal")
+                    .setMessage("Username atau Password salah. Silahkan coba lagi.")
+                    .setPositiveButton("Tutup", null)
+                    .show()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/aliya/MainActivity.kt
+++ app/src/main/java/com/example/aliya/MainActivity.kt
@@ -2,13 +2,16 @@ package com.example.aliya
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.aliya.databinding.ActivityMainBinding
 import com.example.aliya.pertemuan_4.FourthActivity
 import com.example.aliya.pertemuan_5.FifthActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,6 +33,8 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+
+
         // 🔵 KE PERTEMUAN 4
         binding.btnToFourth.setOnClickListener {
             val intent = Intent(this, FourthActivity::class.java)
@@ -43,6 +48,22 @@ class MainActivity : AppCompatActivity() {
         binding.btnToFifth.setOnClickListener {
             val intent = Intent(this, FifthActivity::class.java)
             startActivity(intent)
+        }
+
+        // 🔴 LOGOUT
+        binding.btnLogout.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Ya!")
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
         }
     }
 }
